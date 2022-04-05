6db35828041b24b4d0382b53f9c21ea8597ac864
Huang <mail@huanghong.top>
2022-04-05 17:49:57 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,38 @@
+const webpack = require('webpack')
+const path = require('path')
+const port = 17778
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  publicPath: process.env.PUBLIC_PATH,
+  devServer: {
+    host: 'localhost',
+    //端口号
+    port: port,
+    //自动启动浏览器
+    open: true
+  },
+  configureWebpack: config => {
+    // 生产环境相关配置
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'windows.jQuery': 'jquery'
+      })
+    )
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    })
+
+  },
+  lintOnSave: false,
+  transpileDependencies: [],
+  productionSourceMap: false
 }
