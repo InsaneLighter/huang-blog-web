a03362f3967c890e306f353172dc2a7bbca8be5d
Huang <mail@huanghong.top>
2022-12-06 18:46:07 +0800

    update:评论模块移除非必要内容

--- huang-ui/app/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import HuangUi from '~/index'
-
-const app = createApp(App)
-app.use(HuangUi)
-app.mount('#app')
--- huang-ui/packages/element/tag/index.ts
+++
@@ -1,2 +0,0 @@
-export { ElTag } from 'element-plus'
-import 'element-plus/es/components/tag/style/css'
