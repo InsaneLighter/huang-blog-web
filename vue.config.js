c00aa1d0383f981256c9394c5d463f719643ab8c
Huang <mail@huanghong.top>
2023-03-23 12:49:01 +0800

    update: common update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const webpack = require('webpack')
 const path = require('path')
 const port = 17778
 
+// 在vue-config.js 中加入
+// 开启gzip压缩
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
@@ -34,6 +38,33 @@ module.exports = {
         'windows.jQuery': 'jquery'
       })
     )
+    config.plugins.push(new CompressionWebpackPlugin({
+      algorithm: 'gzip',
+      test: /\.js$|\.html$|\.json$|\.css/,
+      threshold: 10240,
+      minRatio: 0.8
+    }));
+    // 开启分离js
+    config.optimization = {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name (module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`
+            }
+          }
+        }
+      }
+    };
     config.module.rules.push({
       test: /\.mjs$/,
       include: /node_modules/,
